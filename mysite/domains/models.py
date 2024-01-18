075f5998dfe9f1692429b0edde9f14a6afdb4679
jwogrady <john@status26.com>
2024-01-17 20:38:24 -0600

    create model plus index and detail views

--- mysite/domains/models.py
+++ mysite/domains/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class  Domain(models.Model):
+    domain_url = models.CharField(max_length=200)
+    domain_added = models.DateTimeField("The date domain was added to Orbit")
+
+    def __str__(self):
+        return self.domain_url
--- mysite/domains/views.py
+++ mysite/domains/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .models import Domain
+
+class IndexView(generic.ListView):
+    template_name = "domains/index.html"
+    context_object_name = "domain_list"
+
+    def get_queryset(self):
+        """
+        Return a list of domains.
+        """
+        return Domain.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Domain
+    template_name = "domains/detail.html"
